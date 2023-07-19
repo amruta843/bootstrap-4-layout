23f6bc28644ed0262bd38f4a809394d8267a25fd
amruta843 <amrutasamale57@gmail.com>
2023-07-19 18:38:32 +0530

    first comment
    work on b4 layout

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+const cl = console.log;
+
+
+const tmSlider = document.getElementById('tmSlider')
+
+
+$('#tmSlider').owlCarousel({
+    loop:true,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5,
+            margin:20
+        }
+    }
+})
